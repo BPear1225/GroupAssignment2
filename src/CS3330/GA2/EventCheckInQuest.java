4ae01f0a5e9c95f4a20bf42e176e801ab77de438
tizi-23 <tianyizhang0623@gmail.com>
2026-03-16 00:01:53 -0500

    Student Prototype with addPoints and fixed EventCheckInQuest typo

--- src/CS3330/GA2/EventCheckInQuest.java
+++ src/CS3330/GA2/EventCheckInQuest.java
@@ -1,5 +1,30 @@
 package CS3330.GA2;
 
-public class EventCheckInQuest {
+public class EventCheckInQuest extends AbstractQuest {
+    private String eventName;
 
+    public EventCheckInQuest(int id, String title, int basePoints, String eventName) {
+        super(id, title, basePoints);
+        if (eventName == null || eventName.isBlank()) {
+            throw new IllegalArgumentException("eventName must not be null or blank");
+        }
+        this.eventName = eventName;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    @Override
+    public int completeFor(Student s) {
+        isCompleted();
+        int points = getBasePoints();
+        s.addPoints(points);
+        return points;
+    }
+
+    @Override
+    public String toString() {
+        return getId() + ", " + getTitle() + ", " + getBasePoints() + ", " + eventName + ", " + isCompleted();
+    }
 }
---
+++ src/CS3330/GA2/Student.java
@@ -0,0 +1,5 @@
+package CS3330.GA2;
+
+public class Student {
+	public int addPoints(int points) {return 0;}
+}
